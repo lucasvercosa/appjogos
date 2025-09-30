3617698eb2b1041d37e2d95edc82473768042f5e
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2025-09-30 09:55:00 -0300

    pp

---
+++ jogo/src/teste/janela.java
@@ -0,0 +1,13 @@
+package teste;
+
+import javax.swing.JFrame;
+
+public class janela {
+
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("janela");
+
+        janela.setBounds(0,0,1920,1080);
+    }
+}
+
