8b9454673fc08ee056cb6b627e01fa648bb04374
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2025-10-01 09:55:09 -0300

    qqq

---
+++ jogo/src/interface/Janela.java
@@ -0,0 +1,17 @@
+package interface;
+
+import javax.swing.JPanel;
+
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+
+public class Janela extends JFrame{
+    public Janela(){
+        this.setBounds(0,0,1920,1080);
+        CardLayout controleTela = new CardLayout();
+        JPanel telas = new JPanel(controleTela);
+        LoginPainel LoginPainel = new LoginPainel(telas, this);
+        PrincipalPainel PrincipalPainel = new PrincipalPainel(telas, this);
+    }
+}
